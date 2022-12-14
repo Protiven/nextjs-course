1c348f71032357dab8cc8564b19e064e2cf85303
vostrikovva <vostrikovva98@gmail.com>
2022-12-14 12:31:30 +0300

    little changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
+import { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
+
+const App: NextPage<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => NProgress.start())
+    router.events.on('routeChangeComplete', () => NProgress.done())
+    router.events.on('routeChangeError', () => NProgress.done())
+  }, [])
+
   return <Component {...pageProps} />
 }
+
+export default App
